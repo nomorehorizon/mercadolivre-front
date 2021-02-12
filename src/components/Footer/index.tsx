da99ab9c930882318d00f7513802bb7b980d1415
Lucas <lmachado72@outlook.com>
2021-02-12 18:58:03 -0300

    Footer/Header created

---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import { Container } from './styles';
+
+const Footer: React.FC = () => {
+  return (
+    <Container>
+      Cringe
+    </Container>
+  );
+}
+
+export default Footer;
---
+++ src/components/Header/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import { Container } from './styles';
+
+const Header: React.FC = () => {
+  return (
+    <Container>
+      Cringe
+    </Container>
+  );
+}
+
+export default Header;
